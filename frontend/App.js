846701a5d28985e8336b0650192b6c68c60baca8
Kasthoo2000 <87089525+Kasthoo2000@users.noreply.github.com>
2024-05-19 21:18:26 +0530

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,19 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import SignUp from './components/SignUp';
+import Login from './components/Login';
+
+// Create a stack navigator
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SignUp/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name="SignUp" component={SignUp} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#fff' ,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
   },
 });
