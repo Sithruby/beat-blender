e113dcbebfcc07c82b58e26c85ec6cd709bfef31
Sithruby <sithruby@gmail.com>
2024-06-01 16:52:01 +0530

    Added navigation

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
+
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from './components/WelcomeScreen';
 import SignUp from './components/SignUp';
+import Login from "./components/Login";
+
+const Stack=createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SignUp/>
-    </View>
+    <NavigationContainer>
+            <Stack.Navigator initialRouteName="WelcomeScreen" screenOptions={{headerShown:false,}}>
+                <Stack.Screen name="WelcomeScreen" options={{HeaderShown:false}} component={WelcomeScreen}/>
+                <Stack.Screen name="Login" options={{HeaderShown:false}} component={Login}/>
+                <Stack.Screen name="SignUp" options={{HeaderShown:false}} component={SignUp}/>
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 }
 
