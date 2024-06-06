2ceb01b33cfa88a474d7ed504cb90b7cb3f96c90
Sithruby <sithruby@gmail.com>
2024-06-06 12:50:52 +0530

    Added user session management

--- frontend/App.js
+++ frontend/App.js
@@ -1,25 +1,36 @@
 
 import { StyleSheet, Text, View } from 'react-native';
-
-import React from "react";
+import { auth } from './firebase.config';
+import React,{useEffect,useState} from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from './components/WelcomeScreen';
 import SignUp from './components/SignUp';
 import Login from "./components/Login";
+import Home from "./components/Home";
 
 const Stack=createNativeStackNavigator();
 export default function App() {
+  const [currentUser,setCurrentUser]=useState('')
+  useEffect(()=>{
+    const unsub=auth.onAuthStateChanged(()=>{
+      setCurrentUser(auth.currentUser)
+      console.log(currentUser)
+    })
+    return ()=>unsub()
+  },[])
   return (
     <NavigationContainer>
             <Stack.Navigator initialRouteName="WelcomeScreen" screenOptions={{headerShown:false,}}>
                 <Stack.Screen name="WelcomeScreen" options={{HeaderShown:false}} component={WelcomeScreen}/>
                 <Stack.Screen name="Login" options={{HeaderShown:false}} component={Login}/>
                 <Stack.Screen name="SignUp" options={{HeaderShown:false}} component={SignUp}/>
+                <Stack.Screen name="Home" options={{HeaderShown:false}} component={Home} currentUser={currentUser}/>
             </Stack.Navigator>
         </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
