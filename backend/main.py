dd9db4fbe2530ce0abb05f7b2fc5058384d59890
Sithruby <sithruby@gmail.com>
2024-06-07 17:53:07 +0530

    Added route

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,24 @@
-from flask import Flask
+# Filename - main.py
+
+# Import flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    # Retrieve JSON data from the request
+    form_data = request.json
+
+    # Process the form data
+    filepath = form_data.get('uri')
+
+    # Print received form data
+    print('Received file URL:', filepath)
+
+    return jsonify({'message': 'File URL received', 'url': filepath})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=812)
