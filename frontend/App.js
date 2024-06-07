baf1e3ca728aa9865ef48edd2527b7462aa15732
Sithruby <sithruby@gmail.com>
2024-06-07 17:53:25 +0530

    Changed navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,42 +1,41 @@
-
 import { StyleSheet, Text, View } from 'react-native';
 import { auth } from './firebase.config';
-import React,{useEffect,useState} from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from './components/WelcomeScreen';
 import SignUp from './components/SignUp';
-import Login from "./components/Login";
-import Home from "./components/Home";
+import Login from './components/Login';
+import Home from './components/Home';
 
-const Stack=createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 export default function App() {
-  const [currentUser,setCurrentUser]=useState('')
-  useEffect(()=>{
-    const unsub=auth.onAuthStateChanged(()=>{
-      setCurrentUser(auth.currentUser)
-      console.log(currentUser)
-    })
-    return ()=>unsub()
-  },[])
-  return (
-    <NavigationContainer>
-            <Stack.Navigator initialRouteName="WelcomeScreen" screenOptions={{headerShown:false,}}>
-                <Stack.Screen name="WelcomeScreen" options={{HeaderShown:false}} component={WelcomeScreen}/>
-                <Stack.Screen name="Login" options={{HeaderShown:false}} component={Login}/>
-                <Stack.Screen name="SignUp" options={{HeaderShown:false}} component={SignUp}/>
-                <Stack.Screen name="Home" options={{HeaderShown:false}} component={Home} currentUser={currentUser}/>
-            </Stack.Navigator>
-        </NavigationContainer>
-  );
+	const [currentUser, setCurrentUser] = useState('');
+	useEffect(() => {
+		const unsub = auth.onAuthStateChanged(() => {
+			setCurrentUser(auth.currentUser);
+			console.log(currentUser);
+		});
+		return () => unsub();
+	}, []);
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
+				<Stack.Screen name='Home' options={{ HeaderShown: false }} component={Home} />
+				{/* <Stack.Screen name="Home" options={{HeaderShown:false}} component={Home} currentUser={currentUser}/> */}
+				<Stack.Screen name='WelcomeScreen' options={{ HeaderShown: false }} component={WelcomeScreen} />
+				<Stack.Screen name='Login' options={{ HeaderShown: false }} component={Login} />
+				<Stack.Screen name='SignUp' options={{ HeaderShown: false }} component={SignUp} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
 
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:'#fff' ,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
