0d76042e259ee0406899de55ac057fb17b683ef9
Sithruby <sithruby@gmail.com>
2024-06-12 15:00:19 +0530

    Added drawer navigation

--- frontend/App.js
+++ frontend/App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
 import { auth } from './firebase.config';
 import React, { useEffect, useState } from 'react';
@@ -7,7 +8,7 @@ import WelcomeScreen from './components/WelcomeScreen';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import Home from './components/Home';
-
+import Navigation from './components/Navigation';
 const Stack = createNativeStackNavigator();
 export default function App() {
 	const [currentUser, setCurrentUser] = useState('');
@@ -20,12 +21,13 @@ export default function App() {
 	}, []);
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
-				<Stack.Screen name='Home' options={{ HeaderShown: false }} component={Home} />
-				{/* <Stack.Screen name="Home" options={{HeaderShown:false}} component={Home} currentUser={currentUser}/> */}
+			<Stack.Navigator initialRouteName='Navigation' screenOptions={{ headerShown: false }}>
 				<Stack.Screen name='WelcomeScreen' options={{ HeaderShown: false }} component={WelcomeScreen} />
 				<Stack.Screen name='Login' options={{ HeaderShown: false }} component={Login} />
 				<Stack.Screen name='SignUp' options={{ HeaderShown: false }} component={SignUp} />
+				{/* <Stack.Screen name='Home' options={{ HeaderShown: false }} component={Home} /> */}
+				<Stack.Screen name='Home' options={{ HeaderShown: false }} component={Home} currentUser={currentUser} />
+				<Stack.Screen name='Navigation' options={{ HeaderShown: false }} component={Navigation} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
