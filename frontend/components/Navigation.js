410cfe62e779c52b7bc02dd82772011d251ec382
Sithruby <sithruby@gmail.com>
2024-06-13 19:47:24 +0530

    Added tab navigation

--- frontend/components/Navigation.js
+++ frontend/components/Navigation.js
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { Button, View } from 'react-native';
-import { auth, db } from '../firebase.config';
-import { collection, getDoc, getDocs, query, where } from 'firebase/firestore';
-import { signOut } from 'firebase/auth';
-import { Alert } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './Home';
+import Genreclassify from './Genreclassify';
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 function NotificationsScreen({ navigation }) {
 	return (
@@ -14,14 +14,26 @@ function NotificationsScreen({ navigation }) {
 		</View>
 	);
 }
+const Tab = createBottomTabNavigator();
+function TabScreen() {
+	return (
+		<Tab.Navigator>
+			<Tab.Screen name='home' component={Home} options={{ headerShown: false }} />
+			<Tab.Screen name='classify' component={Genreclassify} />
+		</Tab.Navigator>
+	);
+}
 
 const Drawer = createDrawerNavigator();
 
 export default function Navigation() {
 	return (
-		<Drawer.Navigator initialRouteName='Home'>
-			<Drawer.Screen name='Home' component={Home} />
-			<Drawer.Screen name='Notifications' component={NotificationsScreen} />
-		</Drawer.Navigator>
+		<GestureHandlerRootView style={{ flex: 1 }}>
+			<Drawer.Navigator initialRouteName='Home'>
+				<Drawer.Screen name='Home' component={TabScreen} />
+				<Drawer.Screen name='SignOut' component={NotificationsScreen} />
+				<Drawer.Screen name='Classify' component={Genreclassify} />
+			</Drawer.Navigator>
+		</GestureHandlerRootView>
 	);
 }
