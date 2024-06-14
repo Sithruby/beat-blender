def5025602cd9db8e72bc25881ea5c9eb96fc506
Sithruby <sithruby@gmail.com>
2024-06-14 22:56:36 +0530

    Updated firebase config

--- frontend/firebase.config.js
+++ frontend/firebase.config.js
@@ -1,21 +1,20 @@
 import { initializeApp } from 'firebase/app';
-import {initializeAuth} from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
 import { getReactNativePersistence } from '@firebase/auth/dist/rn/index.js';
 import { getFirestore } from 'firebase/firestore';
+
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const firebaseConfig={
-    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket:process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSADING_SENDER_ID,
-    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
-}
+const firebaseConfig = {
+	apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSADING_SENDER_ID,
+	appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+};
 const app = initializeApp(firebaseConfig);
 export const auth = initializeAuth(app, {
 	persistence: getReactNativePersistence(AsyncStorage),
 });
 export const db = getFirestore(app);
-
-
