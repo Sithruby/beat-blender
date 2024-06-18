f763e4d874713b3b9462adebafe00638dde766e2
Sithruby <sithruby@gmail.com>
2024-06-18 20:42:53 +0530

    Added pausing to audio

--- frontend/components/MusicPlayer.js
+++ frontend/components/MusicPlayer.js
@@ -1,40 +1,63 @@
 import { useEffect, useState } from 'react';
-import { View, Text, SafeAreaView, Button, StyleSheet, Alert, Dimensions } from 'react-native';
+import { View, Text, SafeAreaView, Button, StyleSheet, Alert, Dimensions, Pressable } from 'react-native';
 import { Audio } from 'expo-av';
 
 export default function MusicPlayer({ audio_file }) {
-	const [sound, setSound] = useState();
+	const [sound, setSound] = useState(null);
+	const [isPlaying, setIsPlaying] = useState(false);
 
-	async function playSound() {
-		console.log(audio_file);
-		console.log('Loading Sound');
-		const { sound } = await Audio.Sound.createAsync({ uri: audio_file });
-		setSound(sound);
+	async function toggleSound() {
+		if (sound) {
+			if (isPlaying) {
+				await sound.pauseAsync();
+				setIsPlaying(false);
+			} else {
+				await sound.playAsync();
+				setIsPlaying(true);
+			}
+		} else {
+			console.log(audio_file);
+			console.log('Loading Sound');
+			const { sound } = await Audio.Sound.createAsync({ uri: audio_file });
+			setSound(sound);
 
-		console.log('Playing Sound');
-		await sound.playAsync();
+			console.log('Playing Sound');
+			await sound.playAsync();
+			setIsPlaying(true);
+		}
 	}
 
 	useEffect(() => {
 		return sound
-			? () => {
+			? async () => {
 					console.log('Unloading Sound');
-					sound.unloadAsync();
+					await sound.unloadAsync();
 			  }
 			: undefined;
 	}, [sound]);
 
 	return (
-		<View style={styles.container}>
-			<Button title='Play Sound' onPress={playSound} />
-		</View>
+		<Pressable style={styles.button} onPress={toggleSound}>
+			<Text style={styles.buttonText}>{isPlaying ? 'Pause Song' : 'Play Song'}</Text>
+		</Pressable>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'center',
-		backgroundColor: 'transparent',
+	button: {
+		backgroundColor: '#BF05F2',
 		padding: 10,
+		borderRadius: 5,
+		borderColor: 'rgb(0,0,0)',
+		borderWidth: 1,
+		paddingHorizontal: 30,
+		cursor: 'pointer',
+		margin: 30,
+	},
+	buttonText: {
+		fontFamily: 'Inter-SemiBold',
+		color: '#fff',
+		fontSize: 15,
+		fontWeight: 'bold',
 	},
 });
